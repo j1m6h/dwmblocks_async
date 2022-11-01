1d47bac793861eab3c1d9a5fce46a408d5930adf
Joshua <joshuamatthewhagen@yahoo.com>
2022-11-01 02:42:31 -0500

    Use my block scripts.

--- config.h
+++ config.h
@@ -3,14 +3,12 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+        BLOCK("~/.local/bin/sb_volume",         0,      10),
+        BLOCK("~/.local/bin/sb_pkgs",           60,     1),
+        BLOCK("~/.local/bin/sb_memory",         2,      2),
+        BLOCK("~/.local/bin/sb_cputemp",        1,      3),
+        BLOCK("~/.local/bin/sb_nvmeinfo",       2,      4),
+        BLOCK("~/.local/bin/sb_uptime",         60,     5),
+        BLOCK("~/.local/bin/sb_date",           1,      6),
+
 };
