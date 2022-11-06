f37e94f1706e7e2ade77850526a7122efc1a57b0
Joshua <joshuamatthewhagen@yahoo.com>
2022-11-06 17:51:54 -0600

    Add sb_kernel script to display kernel version

--- config.h
+++ config.h
@@ -3,11 +3,11 @@
 
 const Block blocks[] = {
         BLOCK("~/.local/bin/sb_volume",         0,      10),
-        BLOCK("~/.local/bin/sb_pkgs",           60,     1),
-        BLOCK("~/.local/bin/sb_memory",         2,      2),
-        BLOCK("~/.local/bin/sb_cputemp",        1,      3),
-        BLOCK("~/.local/bin/sb_nvmeinfo",       2,      4),
-        BLOCK("~/.local/bin/sb_uptime",         60,     5),
-        BLOCK("~/.local/bin/sb_date",           1,      6),
-
+        BLOCK("~/.local/bin/sb_kernel",		0,      1),
+	BLOCK("~/.local/bin/sb_pkgs",           60,     2),
+        BLOCK("~/.local/bin/sb_memory",         2,      3),
+        BLOCK("~/.local/bin/sb_cputemp",        1,      4),
+        BLOCK("~/.local/bin/sb_nvmeinfo",       2,      5),
+        BLOCK("~/.local/bin/sb_uptime",         60,     6),
+        BLOCK("~/.local/bin/sb_date",           1,      7),
 };
